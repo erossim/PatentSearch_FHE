6ed89ab61fc446aeea66081d87ba531b71980098
erossim <erossim@users.noreply.github.com>
2025-11-11 12:16:50 +0000

    Add service-environment.java, static-common-configuration.rs

---
+++ dynamic-environment-demo/service-environment.java
@@ -0,0 +1,3 @@
+// Auto-generated file: /Users/kevinguoquan/Desktop/zama备用文件/测试/Zama项目/patentsearch_fhe/dynamic-environment-demo/service-environment.java
+// Generated at: 2025-11-27T12:25:21.722Z
+
